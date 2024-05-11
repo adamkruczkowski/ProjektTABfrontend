efbce86b84ec068a9e272065ac6b089f9446d008
Łukasz Jarząb <lukasz.jarzab01@gmail.com>
2024-05-11 23:15:28 +0200

    fixed root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Input } from "@nextui-org/input";
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center h-full flex-1 my-auto ">
-      <h3 className="text-4xl font-bold">
+    <div className="flex flex-col gap-4 items-center justify-center h-full flex-1 my-auto">
+      <h3 className="text-4xl font-bold text-center">
         Witaj w banku największych możliwości
       </h3>
       <h4 className="text-2xl">Załóż konto już teraz!</h4>
